4fd3a32b778ffe55457388ea447fbb377a81e00b
Anastasia Gorlina <gobi199@yandex.ru>
2023-07-15 22:07:32 +0300

    14 pr

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,32 +1,33 @@
 const Card = require('../models/card');
 const NotFoundError = require('../errors/not-found-error');
 const BadRequestError = require('../errors/bad-request-error');
-// возвращает все карточки
-module.exports.getCards = (request, response) => Card.find({})
-  .then((cards) => response.status(200).send({ data: cards }))
-  .catch(() => response.status(500).send({ message: 'Произошла ошибка' }));
+const ForbiddenError = require('../errors/forbidden-error');
 
+// возвращает все карточки
+module.exports.getCards = (request, response, next) => Card.find({})
+  .then((cards) => response.status(200).send({ data: cards }))
+  .catch(next);
+
+// удаляет карточку по _id
 module.exports.deleteCard = (request, response, next) => {
-  const idCard = request.params.id;
-  return Card.findByIdAndRemove(idCard)
-    .then((cardFound) => {
-      if (!cardFound) {
-        throw new NotFoundError(`Карточка с id ${idCard} не найдена`);
+  const { id } = request.params;
+  
+  Card.findById(id)
+    .orFail(() => new NotFoundError(`Карточка с id ${id} не найдена`))
+    .then((card) => {
+      if (!card.owner.equals(request.user._id)) {
+        return next(new ForbiddenError('Недостаточно прав для удаления этой карточки'));
       }
-      return response.status(200).json(cardFound);
+      return card.remove()
+        .then(() => response.send('Карточка удалена'));
     })
-    .catch((error) => {
-      if (error.name === 'CastError') {
-        next(new BadRequestError(`Переданный id ${idCard} не корректен`));
-      } else {
-        next(error); // Для всех остальных ошибок
-      }
-    });
+    .catch(next);
 };
+
 // создаёт карточку
 module.exports.createCard = (request, response, next) => {
   const { name, link } = request.body; // получим из объекта запроса название и ссылку карточки
-  const owner = request.user._id; // временное решение из app.js 20;
+  const owner = request.user._id;
   return Card.create({ name, link, owner }) // создадим карточку на основе пришедших данных
     .then((card) => response.status(201).send(card)) // вернём записанные в базу данные
     .catch((error) => {
--- controllers/users.js
+++ controllers/users.js
@@ -1,15 +1,18 @@
+const bcrypt = require('bcryptjs'); // импортируем bcrypt
+const jwt = require('jsonwebtoken'); // импортируем модуль jsonwebtoken
 const User = require('../models/user');
 const NotFoundError = require('../errors/not-found-error');
 const BadRequestError = require('../errors/bad-request-error');
 const ConflictError = require('../errors/conflict-error');
 
 // возвращает всех пользователей
-module.exports.getUsers = (request, response) => User.find({})
+module.exports.getUsers = (request, response, next) => User.find({})
   .then((users) => response.status(200).send({ data: users }))
-  .catch(() => response.status(500).send({ message: 'Произошла ошибка' }));
+  .catch(next);
 
 // возвращает пользователя по _id
 module.exports.getUserId = (request, response, next) => {
+  // console.log(request.params.id)
   const idUser = request.params.id;
   User.findById(idUser)
     .then((userFound) => {
@@ -29,26 +32,72 @@ module.exports.getUserId = (request, response, next) => {
     });
 };
 
-// создание пользователя
+// создание пользователя, signup
 module.exports.createUser = (request, response, next) => {
-  const { name, about, avatar } = request.body;
-  return User.create({ name, about, avatar }) // создадим пользователя на основе пришедших данных
-    .then((user) => response.status(201).send(
-      {
-        name: user.name,
-        about: user.about,
-        avatar: user.avatar,
-      },
-    ))
+  // console.log('request.body', request.body)
+  // получим из объекта запроса имя, описание, аватар пользователя
+  const {
+    name, about, avatar, email, password,
+  } = request.body;
+  bcrypt
+    .hash(password, 10)
+    .then((hash) => User.create({
+      name, about, avatar, email, password: hash,
+    })) // создадим пользователя на основе пришедших данных
+    .then((user) => response.status(201).send(user))
     .catch((error) => {
+      console.log(error.name);
       if (error.name === 'ValidationError') {
         next(new BadRequestError(`${Object.values(error.errors).map((err) => err.message).join(', ')}`));
-      } else if (error.code === 409) {
-        next(new ConflictError('Пользователь с таким именем уже существует'));
+      } else if (error.code === 11000) {
+        next(new ConflictError('Пользователь с таким email уже существует'));
       } else {
         next(error); // Для всех остальных ошибок
       }
     });
+};
+
+// login
+module.exports.login = (request, response, next) => {
+  const { email, password } = request.body;
+  // console.log(request.body);
+  return User.findUserByCredentials(email, password)
+    .then((user) => {
+      // создадим токен
+      const token = jwt.sign({ _id: user._id }, 'super-strong-secret');
+      // вернём токен
+      response
+        .cookie('jwt', token, {
+          maxAge: 3600000 * 24 * 7,
+          httpOnly: true,
+          sameSite: true,
+        })
+        .send({ data: user.toJSON() });
+    })
+    .catch(next);
+};
+
+module.exports.signout = (req, res) => {
+  res
+    .status(200)
+    .clearCookie('jwt')
+    .send({ message: 'Выход' });
+};
+
+// информация о текущем пользователе
+module.exports.getUser = (request, response, next) => {
+  const userId = request.user._id;
+  console.log(request.user);
+  User.findById(userId)
+    .then((user) => {
+      if (user) {
+        return response.send({
+          data: user,
+        });
+      }
+      throw new NotFoundError('Пользователь по указанному id не найден');
+    })
+    .catch(next);
 };
 
 // обновление профиля
@@ -56,7 +105,7 @@ module.exports.updateUser = (request, response, next) => User.findByIdAndUpdate(
   // console.log(request.user._id),
   request.user._id,
   { name: request.body.name, about: request.body.about },
-  { new: true }, // обработчик then получит на вход обновлённую запись
+  { new: true, runValidators: true }, // обработчик then получит на вход обновлённую запись
 )
   .then((userUpdate) => {
     if (!userUpdate) {
@@ -71,11 +120,12 @@ module.exports.updateUser = (request, response, next) => User.findByIdAndUpdate(
       next(error); // Для всех остальных ошибок
     }
   });
+
 // обновление аватара
 module.exports.updateAvatar = (request, response, next) => User.findByIdAndUpdate(
   request.user._id,
   { avatar: request.body.avatar },
-  { new: true }, // обработчик then получит на вход обновлённую запись
+  { new: true, runValidators: true }, // обработчик then получит на вход обновлённую запись
 )
   .then((avatarUpdate) => {
     if (!avatarUpdate) {
